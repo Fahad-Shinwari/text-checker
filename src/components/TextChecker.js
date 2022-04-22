55dbb4e2b4bb19acca8030e147d8760c8c6b96fa
Fahad-Shinwari <shinwarifahad96@gmail.com>
2022-04-23 00:00:54 +0500

    first commit

---
+++ src/components/TextChecker.js
@@ -0,0 +1,38 @@
+import React,{useState,useEffect} from 'react'
+
+function TextChecker() {
+  const [first, setfirst] = useState("")
+  const [second, setsecond] = useState("")
+  const [wordOne, setwordOne] = useState(0)
+  const [wordTwo, setwordTwo] = useState(0)
+  const [heading, setheading] = useState("")
+
+  useEffect(() => {
+    if(first.length == 0 || second.length == 0){
+      setheading("Type Something in both of these fields")
+    }else if(first==second){
+      setheading("Both fields are same")
+    }else {
+      setheading("They don't Match")
+    }
+    setwordOne(first.split(' ').filter(word=>word !== '').length)
+    setwordTwo(second.split(' ').filter(word=>word !== '').length)
+  }, [first,second])
+  
+
+  return (
+    <div className='text-checker'>
+      <div className='textarea'>
+        <h1>{heading}</h1>
+        <label>Text Area 1:</label>
+        <textarea rows="4" className="form-control" onChange={e=>setfirst(e.target.value.toLowerCase())}></textarea>
+        <p>{`Words Count = ${wordOne}`}</p>
+        <label>Text Area 2</label>
+        <textarea rows="4" className="form-control" onChange={e=>setsecond(e.target.value.toLowerCase())}></textarea>
+        <p>{`Words Count = ${wordTwo}`}</p>
+      </div>
+    </div>
+  )
+}
+
+export default TextChecker
